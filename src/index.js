33b7d0bddcdf24fe51aa28898a1346cba30a4953
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2025-05-25 19:18:16 +0530

    Fix: remove bad code

--- src/index.js
+++ src/index.js
@@ -13,31 +13,10 @@ app.use('/api', apiRoutes)
 
 
 
-app.listen(ServerConfig.PORT,async () => {
+app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
    // Logger.info("Successfully started the server",  {})
-
-   const {City, Airport} = require('./models')
-//     const delhi = await City.findByPk(2);
-//    console.log(delhi);
-//    const city = await City.findByPk(2)
-//    await city.createAirport({name:"Indera gandi airport", code:"NDLS"})
-//    //const airport = await Airport.create({name:'Chaudhari charan singh airport', code:"LKO", cityId:1})
-//  // const Airforce = await lucknow.createAirport({name:'Airforce airport', code:'IAF' })
-// //    console.log(chaudhari);
-//    const airportInLko = await lucknow.getAirports()
-//    console.log(airportInLko);
-    
-// const Airforce = await Airport.findByPk(11)
-// console.log(Airforce);
-// await lucknow.removeAirport(Airforce)
-
- await City.destroy({
-    where: {
-        id: 2
-    }
  })
 
       
     
-})
