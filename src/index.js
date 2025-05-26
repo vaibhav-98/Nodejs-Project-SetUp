4fc8afa2cdd2618f9e9516721d209de400f2bd39
vaibahv-98 <vaibhavtripathi1512@gmail.com>
2025-05-26 14:04:45 +0530

    Added new migration to add foreign key constraint in flight model

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ app.use('/api', apiRoutes)
 
 
 
-
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
    // Logger.info("Successfully started the server",  {})
